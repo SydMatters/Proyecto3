c2e915c578bfb99c387e6f56a080111322435057
juanbedoya51 <66094247+juanbedoya51@users.noreply.github.com>
2020-05-31 13:06:53 -0500

    Update PACIENTES_H.h

--- PACIENTES_H.h
+++ PACIENTES_H.h
@@ -1,3 +1,5 @@
+#define CLS system("cls");
+#include <stdio.h>
 struct pacientes {
     char nombreCompleto [60];
     int id;
@@ -8,3 +10,27 @@ struct pacientes {
 
 char dias[][4]={"LUN","MAR","MIE","JUE","VIE"};
 short horas[]={7,8,9,10,11,2,3,4,5,6};
+
+void salir()
+{
+    CLS;
+    printf("******************************************************************");
+    printf("\n*                                                                *");
+    printf("\n*                     VUELVA PRONTO                              *");
+    printf("\n*                                                                *\n");
+    printf("******************************************************************");
+}
+
+char menu1()
+{
+    char resp;
+    printf("--------------Consultoria Odontologico----------------");
+    printf("\na) Registro de paciente.");
+    printf("\nb) Citas agendadas en la semana.");
+    printf("\nc) Promedios de pacientes.");
+    printf("\nz) Salir.");
+    printf("\n\n A continuacion digite la opcion deseada:");
+    scanf("%c",&resp);
+
+    return resp;
+}
